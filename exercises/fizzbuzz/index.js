3df2b1827f28ce4ad6dafbc77a02af92ed52804c
Shreyas Parbat <shreyasp.2016@sis.smu.edu.sg>
2019-07-08 22:19:59 +0800

    finished fizzbuzz

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,29 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log('fizzbuzz')
+        } else if (i % 3 === 0) {
+            console.log('fizz')
+        } else if (i % 5 === 0) {
+            console.log('buzz');
+        } else {
+            console.log(i)
+        }
+    }
+}
+
+// function fizzBuzz(n) {
+//     for (let i = 1; i <= n; i++) {
+//         let fizz = i % 3 === 0
+//         let buzz = i % 5 === 0        
+//         fizz && buzz ? console.log('fizzbuzz')
+//             : fizz ? console.log('fizz')
+//             : buzz ? console.log('buzz')
+//             : console.log(i);
+//     }
+// }
 
 module.exports = fizzBuzz;
