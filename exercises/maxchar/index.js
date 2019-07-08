9379469aeb80236da9ac7ad8c9c6e792c19771f3
Shreyas Parbat <shreyasp.2016@sis.smu.edu.sg>
2019-07-08 21:48:35 +0800

    completed maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,24 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    chars = str.split('')
+    let maxChar = chars[0]
+    let maxCount = 0
+    let record = {}
+    chars.forEach(char => {
+        console.log(char)
+        if (char in record) {
+            record[char] += 1
+        } else {
+            record[char] = 1
+        }
+        if (record[char] > maxCount) {
+            maxCount = record[char]
+            maxChar = char
+        }
+    })
+    return maxChar
+}
 
 module.exports = maxChar;
