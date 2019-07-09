77c8dbb787289463609a0e8733c0bfd33212be82
Shreyas Parbat <shreyasp.2016@sis.smu.edu.sg>
2019-07-09 12:03:18 +0800

    finished capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,12 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+    let returnString = ''
+    for (let word of str.split(' ')) {
+        returnString += word.substring(0, 1).toUpperCase() + word.substring(1, word.length) + ' '
+    }
+    return returnString.substring(0, returnString.length - 1)
+}
 
 module.exports = capitalize;
