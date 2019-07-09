c911290c4322dc43b8ca237240364e2c4e4a1891
Shreyas Parbat <shreyasp.2016@sis.smu.edu.sg>
2019-07-09 23:07:42 +0800

    finished vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,9 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    array = str.match(/[a+e+i+o+u+A+E+I+O+U]/g)
+    return array ? array.length : 0
+}
 
 module.exports = vowels;
