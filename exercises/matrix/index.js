c59c1a221f9b8d55eedcee240f2a58c83d13bc90
Shreyas Parbat <shreyasp.2016@sis.smu.edu.sg>
2019-07-11 22:07:17 +0800

    finished matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,60 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+    let iMax = n - 1
+    let jMax = n - 1
+    let iMin = 1
+    let jMin = 0
+    let elem = 1
+    let array = [...Array(n)].map(e => Array(n))
+    let i = 0
+    let j = 0
+    while (elem <= n * n) {
+        // Right and Down
+        while (j <= jMax) {
+            array[i][j] = elem
+            elem++
+            j++
+        }
+        j--
+        i++
+        while (i <= iMax) {
+            array[i][j] = elem
+            elem++
+            i++
+        }
+        i--
+        j--
 
+        // Left and up
+        while (j >= jMin) {
+            array[i][j] = elem
+            elem++
+            j--
+        }
+        j++
+        i--
+        while(i >= iMin) {
+            array[i][j] = elem
+            elem++
+            i--
+        }
+        i++
+        j++
+
+        // Updating mins and maxs
+        jMax--
+        iMax--
+        jMin++
+        iMin++
+    }
+    return array
+}
+
+console.log(matrix(5))
+
+// Learning: next time, don't rely on console log to see whether the changes you make will
+// have the desired effect and nudge you closer to the right answer! Solve on whiteboard
+// using memory state diagrams
 module.exports = matrix;
