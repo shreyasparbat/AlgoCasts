178a53cd6cb70bc5a9ac56e48dd703deb3451d1d
Shreyas Parbat <shreyasp.2016@sis.smu.edu.sg>
2019-07-21 18:57:41 +0800

    finished levelwidth

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,23 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+    let currentLevel = [root]
+    let nextLevel = []
+    let widths = []
+    while (currentLevel.length > 0) {
+        widths.push(currentLevel.length)
+        for (let node of currentLevel) {
+            nextLevel = nextLevel.concat(node.children)
+        }
+        currentLevel = []
+        // Deep copy
+        for (let node of nextLevel) {
+            currentLevel.push(node)
+        }
+        nextLevel = []
+    }
+    return widths
+}
 
 module.exports = levelWidth;
