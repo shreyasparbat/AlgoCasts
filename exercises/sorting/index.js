6957369a6afce1ae6a1349cec9dcf40c6b293a3e
Shreyas Parbat <shreyasp.2016@sis.smu.edu.sg>
2019-07-22 10:18:06 +0800

    finished merge sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -10,11 +10,23 @@ function selectionSort(arr) {
 }
 
 function mergeSort(arr) {
-
+    if (arr.length === 1) {
+        return arr
+    }
+    mid = arr.length / 2
+    return merge(mergeSort(arr.slice(0, mid)), mergeSort(arr.slice(mid, arr.length)))
 }
 
 function merge(left, right) {
-
+    let results = []
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            results.push(left.shift())
+        } else {
+            results.push(right.shift())
+        }
+    }
+    return results.concat(left.concat(right))
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
